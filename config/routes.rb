70494ec75cc629e811e240a4da8f33b33dfa3a48
Maureenkamau03 <mkaymaureen@gmail.com>
2022-12-04 12:50:04 +0300

    added power routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :heroes, only: [:index, :show]
+
+  resources :powers, only: [:index, :show, :update]
 end
